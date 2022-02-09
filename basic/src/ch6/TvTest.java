026141a8909fbb76e282942ca9e15fa048f6d808
wookyoungK <pd1125@naver.com>
2022-02-09 23:57:33 +0900

    6장 객체지향I(인스턴스사용 및 생성)

--- basic/src/ch6/TvTest.java
+++ basic/src/ch6/TvTest.java
@@ -1,20 +1,21 @@
 package ch6;
 class Tv { 
      // Tv의 속성(멤버변수) 
-     String color;           	// 색상 
-     boolean power;         	// 전원상태(on/off) 
-     int channel;           	// 채널 
+     String color;           	// 참조형변수 
+     boolean power;         	 
+     int channel;           	
 
      // Tv의 기능(메서드) 
-     void power()   { power = !power; }  // TV를 켜거나 끄는 기능을 하는 메서드  
-     void channelUp()   {  ++channel; }  // TV의 채널을 높이는 기능을 하는 메서드 
-     void channelDown() { --channel; }   // TV의 채널을 낮추는 기능을 하는 메서드  
+     void power()   { power = !power; } 
+     void channelUp()   {  ++channel; }  
+     void channelDown() { --channel; }    
 }
 
 class TvTest { 
       public static void main(String args[]) { 
-            Tv t;                  // Tv인스턴스를 참조하기 위한 변수 t를 선언       
-            t = new Tv();          // Tv인스턴스를 생성한다. 
+            Tv t;                  // Tv인스턴스를 참조하기 위한 변수 t를 선언, 메모리에 참조변수 t를 위한 공간 마련 
+            t = new Tv();          // Tv인스턴스를 생성한다. Tv 클래스의 인스턴스가 메모리의 빈 공간에 생성된다.
+            					   // 객체의 주소값이 참조변수 t에 저장된다. -> 참조변수 t를 통해 Tv인스턴스에 접근 가능
             t.channel = 7;         // Tv인스턴스의 멤버변수 channel의 값을 7로 한다. 
             t.channelDown();       // Tv인스턴스의 메서드 channelDown()을 호출한다. 
             System.out.println("현재 채널은 " + t.channel + " 입니다."); 
--- basic/src/ch6/TvTest4.java
+++ basic/src/ch6/TvTest4.java
@@ -17,16 +17,14 @@ class TvTest4 {
 }
 
 
-/*
-class Tv { 
-     // Tv의 속성(멤버변수) 
-     String color;           // 색상 
-     boolean power;          // 전원상태(on/off) 
-     int channel;          	 // 채널 
-
-     // Tv의 기능(메서드) 
-     void power()   { power = !power; }  // TV를 켜거나 끄는 기능을 하는 메서드 
-     void channelUp()   {  ++channel; }  // TV의 채널을 높이는 기능을 하는 메서드 
-     void channelDown() {  --channel; }  // TV의 채널을 낮추는 기능을 하는 메서드  
-}
-*/
+//class Tv { 
+//// Tv의 속성(멤버변수) 
+//String color;          // 색상 
+//boolean power;         // 전원상태(on/off) 
+//int channel;           // 채널 
+//
+//// Tv의 기능(메서드) 
+//void power()   { power = !power; }  
+//void channelUp()   {  ++channel; }  
+//void channelDown() {  --channel; }   
+//}
