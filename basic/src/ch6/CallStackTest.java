5cb59fe50c2a36d5bb0d8373af10e1c2cf0b7dc2
wookyoungK <pd1125@naver.com>
2022-02-12 23:24:07 +0900

    6장 객체지향I(JVM메모리)

---
+++ basic/src/ch6/CallStackTest.java
@@ -0,0 +1,14 @@
+package ch6;
+class CallStackTest {
+	public static void main(String[] args) {
+		firstMethod();
+	}
+
+	static void firstMethod() {
+		secondMethod();
+	}
+
+	static void secondMethod() {
+		System.out.println("secondMethod()");		
+	}
+}
