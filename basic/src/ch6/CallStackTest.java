801cc71f7ab59a156a6a1903fccac4b539bd6368
wookyoungK <pd1125@naver.com>
2022-02-15 01:46:02 +0900

    6장 객체지향I( 메모리 호출 복습)

--- basic/src/ch6/CallStackTest.java
+++ basic/src/ch6/CallStackTest.java
@@ -7,7 +7,7 @@ class CallStackTest {
 	static void firstMethod() {
 		secondMethod();
 	}
-
+	// static 메서드는 객체 생성없이 호출 가능  
 	static void secondMethod() {
 		System.out.println("secondMethod()");		
 	}
--- basic/src/ch6/MyMathTest.java
+++ basic/src/ch6/MyMathTest.java
@@ -1,11 +1,11 @@
 package ch6;
 class MyMathTest {
 	public static void main(String args[]) {
-		MyMath mm = new MyMath();
+		MyMath mm = new MyMath(); //먼저 인스턴스 생
 		long result1 = mm.add(5L, 3L);
 		long result2 = mm.subtract(5L, 3L);
 		long result3 = mm.multiply(5L, 3L);
-		double result4 = mm.divide(5L, 3L);
+		double result4 = mm.divide(5L, 3L); //자동형변환 
 		System.out.println("add(5L, 3L) = "      + result1);
 		System.out.println("subtract(5L, 3L) = " + result2);
 		System.out.println("multiply(5L, 3L) = " + result3);
