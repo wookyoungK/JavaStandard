3a7373ae337156a59623066c8515d49441e956c4
wookyoungK <pd1125@naver.com>
2022-02-27 12:08:41 +0900

    7장 객체지향II(오버로딩,라이딩 - super)

---
+++ basic/src/ch7/SuperTest.java
@@ -0,0 +1,19 @@
+package ch7;
+class SuperTest {
+	public static void main(String args[]) {
+		Child c = new Child();
+		c.method();
+	}
+}
+
+class Parent {
+	int x=10;
+}
+
+class Child extends Parent {
+	void method() {
+		System.out.println("x=" + x);
+		System.out.println("this.x=" + this.x);
+		System.out.println("super.x="+ super.x);
+	}
+}
