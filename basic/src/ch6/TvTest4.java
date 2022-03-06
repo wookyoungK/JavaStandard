2bc43728fe5201d8c185ee779aa8a756c5566e5b
wookyoungK <pd1125@naver.com>
2022-03-06 23:59:02 +0900

    6장 객체지향I(인스턴스 복습)

--- basic/src/ch6/TvTest4.java
+++ basic/src/ch6/TvTest4.java
@@ -2,12 +2,14 @@ package ch6;
 class TvTest4 {
 	public static void main(String args[]) {
 		Tv[] tvArr = new Tv[3]; // 길이가 3인 Tv객체 배열
+		//참조 변수들이 만들어진것일 뿐 객체가 저장된것은 아니다.
 
 		// Tv객체를 생성해서 Tv객체 배열의 각 요소에 저장
 		for(int i=0; i < tvArr.length;i++) {
 			tvArr[i] = new Tv();
 			tvArr[i].channel = i+10; // tvArr[i]의 channel에 i+10을 저장
 		}
+		// Tv[] tvArr = {new Tv(), new Tv(), new Tv() }
 
 		for(int i=0; i < tvArr.length;i++) {
 			tvArr[i].channelUp();  // tvArr[i]의 메서드를 호출. 채널이 1증가
