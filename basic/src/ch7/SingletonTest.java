a22eedebe9f37195ce68a6048d95cffe8c316d4a
wookyoungK <pd1125@naver.com>
2022-03-12 23:59:09 +0900

    7장 객체지향II(생성자의 접근제어자)

---
+++ basic/src/ch7/SingletonTest.java
@@ -0,0 +1,24 @@
+package ch7;
+final class Singleton {
+	private static Singleton s = new Singleton();
+	
+	private Singleton() {
+		//...
+	}
+
+	public static Singleton getInstance() {
+		if(s==null) {
+			s = new Singleton();
+		}
+		return s;
+	}	
+
+	//...
+}
+
+class SingletonTest {
+	public static void main(String args[]) {
+//		Singleton s = new Singleton();
+		Singleton s = Singleton.getInstance();
+	}
+}
