9b0c67bcda4970962f73cd01bed1975e61d2c9b8
wookyoungK <pd1125@naver.com>
2022-03-29 02:09:09 +0900

    7장 객체지향II(매개변수의 다형성)

--- basic/src/ch7/PolyArgumentTest.java
+++ basic/src/ch7/PolyArgumentTest.java
@@ -33,15 +33,18 @@ class Computer extends Product {
 class Buyer {			// 고객, 물건을 사는 사람
 	int money = 1000;	// 소유금액
 	int bonusPoint = 0;	// 보너스점수
-
+	Product [] item = new Product[10]; // 여러종류의 객체를 배열로 다루
+	int i =0;
+	
 	void buy(Product p) {
 		if(money < p.price) {
 			System.out.println("잔액이 부족하여 물건을 살수 없습니다.");
 			return;
-		}
+		}	
 
 		money -= p.price;			// 가진 돈에서 구입한 제품의 가격을 뺀다.
 		bonusPoint += p.bonusPoint;	// 제품의 보너스 점수를 추가한다.
+		item[i++] = p;
 		System.out.println(p + "을/를 구입하셨습니다.");
 	}
 }
