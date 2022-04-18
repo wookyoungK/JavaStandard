2fb89295b21b59e2e2e4d6eacb25843227a066cf
wookyoungK <pd1125@naver.com>
2022-04-18 22:34:35 +0900

    7장 객체지향II(default, static Method)

--- basic/src/ch7/DefaultMethodTest.java
+++ basic/src/ch7/DefaultMethodTest.java
@@ -1,29 +1,29 @@
 package ch7;
 class DefaultMethodTest {
 	public static void main(String[] args) {
-		Child c = new Child();
-		c.method1();
+		ChildDefaultMethod c = new ChildDefaultMethod();
+		c.method1(); 
 		c.method2();
-		MyInterface.staticMethod(); 
+		MyInterface.staticMethod(); //인터페이스의 static 메서드 접근가능 
 		MyInterface2.staticMethod();
 	}
 }
 
-class Child extends Parent implements MyInterface, MyInterface2 {
-	public void method1() {	
-		System.out.println("method1() in Child"); // 오버라이딩
-	}			
-}
-
-class Parent {
-	public void method2() {	
-		System.out.println("method2() in Parent");
-	}
-}
+  class ChildDefaultMethod extends ParentChildDefaultMethod implements MyInterface, MyInterface2 { 
+	  public void method1() { 
+		  System.out.println("method1() in Child"); // 오버라이딩 -> 인터페이스 메서드를 오버라이딩 하여 변경가
+	  }
+  }
+  
+  class ParentChildDefaultMethod { 
+	  public void method2() {
+		  System.out.println("method2() in Parent"); //조상클래스의 메서드는 다른 인터페이스 메서드(디폴트 메서드)를 무시한다.
+  	} 
+  }
 
 interface MyInterface {
 	default void method1() { 
-		System.out.println("method1() in MyInterface");
+		System.out.println("method1() in MyInterface"); // 
 	}
 	
 	default void method2() { 
