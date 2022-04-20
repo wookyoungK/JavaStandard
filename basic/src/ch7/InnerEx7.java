4e7534f072c2daf7046b2b90e63065ffa44281de
wookyoungK <pd1125@naver.com>
2022-04-20 22:46:38 +0900

    7장 객체지향II(익명클래스)

---
+++ basic/src/ch7/InnerEx7.java
@@ -0,0 +1,16 @@
+package ch7;
+import java.awt.*;
+import java.awt.event.*;
+
+class  InnerEx7{
+	public static void main(String[] args) {
+		Button b = new Button("Start");
+		b.addActionListener(new EventHandler());
+	}
+}
+
+class EventHandler implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("ActionEvent occurred!!!");
+	}
+}
