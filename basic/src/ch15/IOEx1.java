88ee4730c5feb19b0c9f9c8e9d121f6a7aae6c60
wookyoungK <pd1125@naver.com>
2022-12-15 23:59:57 +0900

    ch15.문자 기반,바이트기반 스트림

---
+++ basic/src/ch15/IOEx1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+
+// 바이트 배열 inSrc의 데이터를 outSrc로 복사하는 예제, read(), write()를 사용하는 가장 기본적인 방법
+class IOEx1 {
+	public static void main(String[] args) {
+		byte[] inSrc = {0,1,2,3,4,5,6,7,8,9};
+		byte[] outSrc = null;
+
+		ByteArrayInputStream  input  = null;
+		ByteArrayOutputStream output = null;
+
+		input  = new ByteArrayInputStream(inSrc);
+		output = new ByteArrayOutputStream();
+
+		int data = 0;
+
+		while((data = input.read())!=-1) {
+			output.write(data);	// void write(int b)
+		}
+
+		outSrc = output.toByteArray(); // 스트림의 내용을 byte배열로 반환한다.
+
+		System.out.println("Input Source  :" + Arrays.toString(inSrc));
+		System.out.println("Output Source :" + Arrays.toString(outSrc));
+	}
+}
